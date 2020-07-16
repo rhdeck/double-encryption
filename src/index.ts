5330c018e93a3607d52b1c8ff2307db94d3415be
Ray Deck <ray@raydeck.com>
2020-07-16 18:59:32 -0500

    Using the new decryptToString API from stream-crypto

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import {
   encrypt,
-  decryptToText,
+  decryptToString,
   decryptToBuffer,
   encryptFile,
   decryptFile,
   makeRandomKeyBuffer,
+  encryptToString,
+  makeRandomKeyString,
 } from "@raydeck/stream-crypto";
 class Manager {
   protected encryptionKey?: string;
@@ -15,6 +17,7 @@ class Manager {
     this.encryptedKey = encryptedKey;
   }
   toObj(): { clientKey: string; encryptedKey: string } {
+    if (!this.clientKey || !this.encryptedKey) throw new Error("No key stored");
     return {
       clientKey: this.clientKey,
       encryptedKey: this.encryptedKey,
@@ -25,11 +28,11 @@ class Manager {
   }
   async encryptText(text: string) {
     const key = await this.getEncryptionKey();
-    return await encrypt(text, key);
+    return await encryptToString(text, key);
   }
-  async decryptText(data: any) {
+  async decryptText(data: string) {
     const key = await this.getEncryptionKey();
-    return await decryptToText(data, key);
+    return await decryptToString(data, key);
   }
   async encryptImage(b: Buffer) {
     const key = await this.getEncryptionKey();
@@ -50,37 +53,26 @@ class Manager {
   async getEncryptionKey() {
     if (!this.encryptionKey) {
       const { clientKey, encryptedKey } = this.toObj();
-      if (!clientKey || !encryptedKey) throw "No key stored";
-      const clientKeyBuffer = Buffer.from(clientKey, "base64");
-      const encryptedKeyBuffer = Buffer.from(encryptedKey, "base64");
-      const encryptionKey = await decryptToBuffer(
-        encryptedKeyBuffer,
-        clientKeyBuffer
-      );
-      this.encryptionKey = encryptionKey.toString("base64");
+      this.encryptionKey = await decryptToString(encryptedKey, clientKey);
     }
     return this.encryptionKey;
   }
-  async setKey(newKey: string | Buffer): Promise<void> {
-    if (!newKey) throw "requires a an argument";
-    if (typeof newKey === "string") newKey = Buffer.from(newKey);
+  async setKey(BufferOrBase64: string | Buffer): Promise<void> {
+    const newKey =
+      typeof BufferOrBase64 === "string"
+        ? Buffer.from(BufferOrBase64, "base64")
+        : BufferOrBase64;
     const encryptionKey = await this.getEncryptionKey();
     if (!encryptionKey) throw "there is no stored key";
-    //Need to re-encrypt
-    const newEncryptedKey = await encrypt(encryptionKey, newKey);
-    const newEncryptedKey64 = newEncryptedKey.toString("base64");
-    const newKey64 = newKey.toString("base64");
-    this.clientKey = newKey64;
-    this.encryptedKey = newEncryptedKey64;
+    const newEncryptedKey = await encryptToString(encryptionKey, newKey);
+    this.clientKey = newKey.toString("base64");
+    this.encryptedKey = newEncryptedKey;
   }
   static async create(length: number = 32): Promise<Manager> {
-    const toEncrypt: Buffer = await makeRandomKeyBuffer(length);
-    const clientKey: Buffer = await makeRandomKeyBuffer(length);
-    const encryptedKey: Buffer = await encrypt(toEncrypt, clientKey);
-    return new Manager(
-      clientKey.toString("base64"),
-      encryptedKey.toString("base64")
-    );
+    const toEncrypt = await makeRandomKeyBuffer(length);
+    const clientKey = await makeRandomKeyString(length);
+    const encryptedKey = await encryptToString(toEncrypt, clientKey);
+    return new Manager(clientKey, encryptedKey);
   }
 }
 
